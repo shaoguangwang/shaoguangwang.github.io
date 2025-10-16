4559b785aaeee6d2ea9597b90aaeb99f3e57ff0b
Shaoguang WANG <shaoguangwang9@gmail.com>
2025-10-16 18:59:50 +0800

    add html page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,8 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards']
+// const section_names = ['home', 'publications', 'awards']
+const section_names = ['home', 'publications', 'awards', 'projects']
 
 
 window.addEventListener('DOMContentLoaded', event => {
